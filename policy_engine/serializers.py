be65c20612fbc177862051c8770e093e7b568daa
Dan Aharoni <aharonidan@gmail.com>
2019-08-09 00:23:56 +0100

    create transaction only if it matches the rules

--- policy_engine/serializers.py
+++ policy_engine/serializers.py
@@ -7,6 +7,13 @@ class TransactionSerializer(serializers.ModelSerializer):
 		model = Transaction
 		fields = ('id', 'amount', 'address', 'status')
 
+	def create(self, validated_data):
+		transaction = super(TransactionSerializer, self).create(validated_data)
+		rules = Rule.objects.filter(whitelist_address__exact=transaction.address).filter(max_amount__gte=transaction.amount)
+		transaction.status = 'rejected' if not rules else 'confirmed'
+
+		return transaction
+
 class RuleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Rule
