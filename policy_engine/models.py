f1e79f2f5ad77bf6c535f1a0cdf6d80bca7e6d78
Dan Aharoni <aharonidan@gmail.com>
2019-08-09 01:33:51 +0100

    support multiple currencies

--- policy_engine/models.py
+++ policy_engine/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Transaction(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
-	amount = models.IntegerField(default=0)
+	amount  = models.FloatField(default=0)
 	address = models.CharField(max_length=34)
 	status = models.CharField(max_length=10, choices=[('confirmed', 'Confirmed'), ('rejected', 'Rejected')])
 
 class Rule(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
-	max_amount = models.IntegerField(default=0)
+	max_amount = models.FloatField(default=0)
 	currency = models.CharField(max_length=10, choices=[('USD', 'USD'), ('BTC', 'BTC')])
 	whitelist_address = models.CharField(max_length=34)
 
--- policy_engine/serializers.py
+++ policy_engine/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Transaction, Rule
+import requests
 
 
 class TransactionSerializer(serializers.ModelSerializer):
@@ -9,7 +10,15 @@ class TransactionSerializer(serializers.ModelSerializer):
 
 	def create(self, validated_data):
 		transaction = super(TransactionSerializer, self).create(validated_data)
-		rules = Rule.objects.filter(whitelist_address__exact=transaction.address).filter(max_amount__gte=transaction.amount)
+
+		ticker = requests.get('https://blockchain.info/ticker').json()
+		usd_amount = ticker['USD']['last'] * transaction.amount
+
+		usd_rules = Rule.objects.filter(whitelist_address__exact=transaction.address).filter(currency__exact='USD').filter(max_amount__gte=usd_amount)
+		btc_rules = Rule.objects.filter(whitelist_address__exact=transaction.address).filter(currency__exact='BTC').filter(max_amount__gte=transaction.amount)
+		rules = usd_rules | btc_rules
+
+
 		transaction.status = 'rejected' if not rules else 'confirmed'
 
 		return transaction
